2f4160b52bdef754e7fddf34b554fcdb020f67be
Andre Melo <andre.fabian.melo@gmail.com>
2020-07-09 02:43:22 -0300

    Add appointment screen

---
+++ src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+// import { Container } from './styles';
+
+const AppointmentCreated: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="sair" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default AppointmentCreated;
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+// import { Container } from './styles';
+
+const Profile: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="sair" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default Profile;
