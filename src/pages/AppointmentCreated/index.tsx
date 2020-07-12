475a8634c40fd4c0360a1525b833104bb6792dc6
Andre Melo <andre.fabian.melo@gmail.com>
2020-07-12 03:12:38 -0300

    Add Appointment Created success screen

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
-// import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="sair" onPress={() => {}} />
-    </View>
+    <Container>
+      <Icon name="check" size={150} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 23 de agosto de 2020 às 12:00h</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
+    </Container>
   );
 };
 
